fefe8e4426e35ea5a4eb7fcfdf59fdafcc81db7b
Mathias17COURNU <mathias.cournu@gmail.com>
2024-11-24 16:20:33 +0100

    Create MAIN.C
    le main.C du programme

---
+++ MAIN.C
@@ -0,0 +1,21 @@
+Main.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <time.h>
+#include "cours.h"
+
+
+
+
+
+// main
+int main() {
+    Menu();
+    return 0;
+}
+
+
+
+//fin
